3d5d5706508ab5b2abcdb13cc27ca5b3de3e66a7
Dami-Lescano <lescano5600@gmail.com>
2024-11-11 15:09:48 -0300

    Listo con Radix. Ahora si.

--- RadixSort/radix_sort.py
+++ RadixSort/radix_sort.py
@@ -1,46 +1,36 @@
 import random
 import time
 
-from matplotlib import pyplot as plt
-
 #--------------------------------------------------------------
 #FUNCIONES EXTRAS
 #--------------------------------------------------------------
 
-
-
-
 def get_digit(number, n):
     return number // 10**n % 10
 
-
-
-
-def generar_lista(n, rango):
-    pregenerada = ""#str(input("Cargar lista pregenerada. "))
+def generar_lista():
+    pregenerada = str(input("Cargar lista pregenerada. "))
     if pregenerada == "s":
-        lista = range(0, 200)
+        lista = [random.randint(0,9999) for _ in range(1000)]
     else:
-        cantidad = n#500#int(input("Ingresar cantidad de numeros. "))
-        limiteMenor = 0#int(input("Ingresar el limite menor del conjunto. "))
-        limiteMayor = rango#999999#int(input("Ingresar el limite mayor del conjunto. "))
-        repetidos = "s"#str(input("Permitir repetidos. "))
+        cantidad = int(input("Ingresar cantidad de numeros. "))
+        limite_menor = 0#int(input("Ingresar el limite menor del conjunto. "))
+        limite_mayor = int(input("Ingresar el limite mayor del conjunto. "))
+        repetidos = str(input("Permitir repetidos. "))
 
 
         if repetidos == "s":
             lista = list()
             for _ in range(cantidad):
-                lista.append(random.randint(limiteMenor, limiteMayor))
+                lista.append(random.randint(limite_menor, limite_mayor))
         else:
             lista = set()
             while len(lista) < cantidad:
-                lista.add(random.randint(limiteMenor, limiteMayor))
+                lista.add(random.randint(limite_menor, limite_mayor))
     
     return list(lista)
 
 #--------------------------------------------------------------
-
-
 
 def radix_sort(digito, lista_a_ordenar):
     lista_de_colas = [[],[],[],[],[],[],[],[],[],[]]
@@ -59,90 +49,23 @@ def radix_sort(digito, lista_a_ordenar):
             elif len(cola) == 1:
                 lista_ordenada.extend(cola)
         return lista_ordenada
-    
 
+lista = generar_lista()
 
-#lista = [13,3,23,11,12,0,11,44,44]
+calculos = 0
 
-listaCalculos = []
-listaTiempos = []
-listaCantidades = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
-maximo = 9999
+print("Lista sin ordenar: ", lista)
 
-for n in listaCantidades:
+tiempo_inicio = time.time()
 
-    lista = generar_lista(n, maximo)
+digitos = len(str(max(lista)))
 
-    calculos = 0
+lista_ordenada_final = radix_sort(digitos-1, lista)
 
-    #print("Lista sin ordenar: ", lista)
+tiempo_final = time.time()
 
-    tiempoInicio = time.time()
+tiempo_total = tiempo_final - tiempo_inicio
 
-    digitos = len(str(max(lista)))
-
-    lista_ordenada_final = radix_sort(digitos-1, lista)
-
-    tiempoFinal = time.time()
-    
-    tiempoTotal = tiempoFinal - tiempoInicio
-
-    listaTiempos.append(tiempoTotal)
-
-    listaCalculos.append(calculos)
-
-    #print(f"Lista ordenada final: {lista_ordenada_final}")
-    #print(f"calculos = {calculos}, digitos * cantidad")
-
-yi = []
-
-for i in listaCantidades:
-    current = digitos*(i)
-    yi.append(current)
-
-print("cantidades: ", listaCantidades)
-print("calculos: ", listaCalculos)
-print("tiempos: ", listaTiempos)
-print("esperado: ", yi)
-
-ancho_barra = 35
-
-xi_sin_concurrencia = []
-xi_con_concurrencia = []
-for i in listaCantidades:
-    xi_sin_concurrencia.append(i - ancho_barra/2)
-    xi_con_concurrencia.append(i + ancho_barra/2)
-
-plt.bar(xi_sin_concurrencia, listaCalculos, width=ancho_barra, color='blue', label='Sin concurrencia')
-plt.bar(xi_con_concurrencia, [280, 608, 980, 1331, 1688, 2067, 2473, 2853, 3243, 3644], width=ancho_barra, color='pink', label='Con concurrencia')
-plt.plot(listaCantidades, yi, color = 'red', label=f'Peor caso = n*d')#k es la base
-
-
-# Etiquetas y título
-plt.xlabel('Cantidad (n)')
-plt.ylabel('Calculos')
-plt.title(f'Radix Sort - d = {digitos}')
-plt.xticks(listaCantidades)  # Configurar las etiquetas del eje x
-plt.legend()  # Mostrar la leyenda
-
-# Mostrar el gráfico
-plt.show()
-
-#-------------------------------------------------------
-
-tiempo_concurrencia = [0.018971920013427734, 0.054999351501464844, 0.05300021171569824, 0.07000017166137695, 0.09000158309936523, 0.10600018501281738, 0.12599849700927734, 0.14021897315979004, 0.16199994087219238, 0.18200325965881348]
-
-plt.bar(xi_sin_concurrencia, listaTiempos, width=ancho_barra, color='blue', label='Sin concurrencia')
-plt.bar(xi_con_concurrencia, tiempo_concurrencia, width=ancho_barra, color='pink', label='Con concurrencia')
-#plt.plot(listaCantidades, yi, color = 'red', label=f'Peor caso = n*d')#k es la base
-
-
-# Etiquetas y título
-plt.title(f'Radix Sort - d = {digitos}')
-plt.xticks(listaCantidades)  # Configurar las etiquetas del eje x
-plt.legend()  # Mostrar la leyenda
-
-# Mostrar el gráfico
-plt.xlabel('Cantidad')
-plt.ylabel('Tiempo')
-plt.show()
+print(f"Lista ordenada final: {lista_ordenada_final}")
+print(f"calculos = {calculos}")
+print(f"Tiempo transcurrido = {tiempo_total}")
--- RadixSort/radix_sort_threaded.py
+++ RadixSort/radix_sort_threaded.py
@@ -1,7 +1,6 @@
 import random
 from threading import *
 import time
-from matplotlib import pyplot as plt
 
 class NewThread(Thread):
     def __init__(self, group=None, target=None, name=None, args=(), kwargs={}):
@@ -19,44 +18,35 @@ class NewThread(Thread):
 #FUNCIONES EXTRAS
 #--------------------------------------------------------------
 
-
-
 def place_holder(c):
     return c
-
-
 
 def get_digit(number, n):
     return number // 10**n % 10
 
-
-
-
-def generar_lista(n):
-    pregenerada = ""#str(input("Cargar lista pregenerada. "))
+def generar_lista():
+    pregenerada = str(input("Cargar lista pregenerada. "))
     if pregenerada == "s":
-        lista = range(0, 200)
+        lista = [random.randint(0,9999) for _ in range(1000)]
     else:
-        cantidad = n#500#int(input("Ingresar cantidad de numeros. "))
-        limiteMenor = 0#int(input("Ingresar el limite menor del conjunto. "))
-        limiteMayor = 9999999#int(input("Ingresar el limite mayor del conjunto. "))
-        repetidos = "s"#str(input("Permitir repetidos. "))
+        cantidad = int(input("Ingresar cantidad de numeros. "))
+        limite_menor = 0#int(input("Ingresar el limite menor del conjunto. "))
+        limite_mayor = int(input("Ingresar el limite mayor del conjunto. "))
+        repetidos = str(input("Permitir repetidos. "))
 
 
         if repetidos == "s":
             lista = list()
             for _ in range(cantidad):
-                lista.append(random.randint(limiteMenor, limiteMayor))
+                lista.append(random.randint(limite_menor, limite_mayor))
         else:
             lista = set()
             while len(lista) < cantidad:
-                lista.add(random.randint(limiteMenor, limiteMayor))
+                lista.add(random.randint(limite_menor, limite_mayor))
     
     return list(lista)
 
 #--------------------------------------------------------------
-
-
 
 def radix_sort(digito, lista_a_ordenar):
     lista_de_colas = [[],[],[],[],[],[],[],[],[],[]]
@@ -83,69 +73,22 @@ def radix_sort(digito, lista_a_ordenar):
             lista_ordenada.extend(t.join())
         return lista_ordenada
 
+lista = generar_lista()
 
+print("Lista sin ordenar: ", lista)
 
+calculos = 0
 
+tiempo_inicio = time.time()
 
-#lista = [13,3,23,11,12,0,11,44,44]
+digitos = len(str(max(lista)))
 
-listaCalculos = []
-listaTiempos = []
-listaCantidades = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+lista_ordenada_final = radix_sort(digitos-1, lista)
 
-for n in listaCantidades:
+tiempo_final = time.time()
 
-    lista = generar_lista(n)
+tiempo_total = tiempo_final - tiempo_inicio
 
-    calculos = 0
-
-    tiempoInicio = time.time()
-
-    digitos = len(str(max(lista)))
-
-    lista_ordenada_final = radix_sort(digitos-1, lista)
-
-    tiempoFinal = time.time()
-    
-    tiempoTotal = tiempoFinal - tiempoInicio
-
-    #print("Lista sin ordenar: ", lista)
-
-    listaCalculos.append(calculos)
-
-    listaTiempos.append(tiempoTotal)
-
-    #print(f"Lista ordenada final: {lista_ordenada_final}")
-    #print(f"calculos = {calculos}, digitos * cantidad")
-
-yi = []
-
-for i in listaCantidades:
-    current = digitos*(i)
-    yi.append(current)
-
-print("cantidades: ", listaCantidades)
-print("calculos: ", listaCalculos)
-print("tiempos: ", listaTiempos)
-print("esperado: ",yi)
-
-ancho_barra = 35
-
-plt.bar(listaCantidades, listaCalculos, width=ancho_barra, color='blue', label='Calculos')
-#plt.bar(xi_con_concurrencia, calculos_con_concurrencia.mean(), width=ancho_barra, color='pink', label='Con concurrencia')
-plt.plot(listaCantidades, yi, color = 'red', label='Peor caso = 4*(n+10)')#k es la base
-
-
-# Etiquetas y título
-plt.xlabel('Cantidad')
-plt.ylabel('Calculos')
-plt.title('Radix Sort')
-plt.xticks(listaCantidades)  # Configurar las etiquetas del eje x
-plt.legend()  # Mostrar la leyenda
-
-# Mostrar el gráfico
-plt.xlabel('Cantidad')
-plt.ylabel('Calculos')
-plt.show()
-
-#-------------------------------------------------------
+print(f"Lista ordenada final: {lista_ordenada_final}")
+print(f"calculos = {calculos}, digitos * cantidad")
+print(f"Tiempo transcurrido = {tiempo_total}")
